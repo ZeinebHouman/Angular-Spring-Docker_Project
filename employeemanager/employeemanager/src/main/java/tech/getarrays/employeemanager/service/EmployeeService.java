adab316ef8fda501357c9ab358a7190218bbed36
LENOVO <zeinebhouman9@gmail.com>
2022-01-29 13:36:04 +0100

    add controller : EmployeeResource

--- employeemanager/employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
+++ employeemanager/employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -2,9 +2,11 @@ package tech.getarrays.employeemanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.Exception.UserNotFoundException;
 import tech.getarrays.employeemanager.model.Employee;
 import tech.getarrays.employeemanager.repository.EmployeeRepo;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -22,4 +24,21 @@ public class EmployeeService {
         return employeeRepo.save(employee);
     }
 
+    public List<Employee> findAllEmployee()
+    {
+        return employeeRepo.findAll();
+    }
+    public Employee updateEmployee(Employee employee)
+    {
+        return employeeRepo.save(employee);
+    }
+    public Employee findEmployeeById(Long id)
+    {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + "was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+         employeeRepo.deleteEmployeeById(id);
+    }
 }
